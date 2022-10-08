73771403ddec9df246a00e6ef3510bb5a2e4ef1c
Kuldeep1998 <68820413+Kuldeep1998@users.noreply.github.com>
2022-10-08 14:13:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,31 +20,31 @@ function events(){
 function makeSound(key){
   switch(key){
     case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
+      var tom1 = new Audio("sounds/t1.mp3");
     return tom1.play();
     break;
     case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
+      var tom2 = new Audio("sounds/t2.mp3");
     return tom2.play();
 break;
     case "s":
-      var tom3 = new Audio("sounds/tom-3.mp3");
+      var tom3 = new Audio("sounds/t3.mp3");
     return tom3.play();
 break;
     case "d":
-      var tom4 = new Audio("sounds/tom-4.mp3");
+      var tom4 = new Audio("sounds/t4.mp3");
     return tom4.play();
 break;
     case "j":
-      var snare = new Audio("sounds/snare.mp3");
+      var snare = new Audio("sounds/snar.mp3");
     return snare.play();
 break;
     case "k":
-      var crash = new Audio("sounds/crash.mp3");
+      var crash = new Audio("sounds/crashed.mp3");
     return crash.play();
 break;
     case "l":
-      var kickBass = new Audio("sounds/kick-bass.mp3");
+      var kickBass = new Audio("sounds/bass.mp3");
     return kickBass.play();
 break;
 
